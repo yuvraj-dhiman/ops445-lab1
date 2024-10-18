2eddf3bed48330870210cf07d3dae07c21142138
Yuvraj-dhiman <yuvraj-dhiman@myseneca.ca>
2024-10-18 15:47:06 -0400

    Added lab1a

---
+++ lab1a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+# Any line that starts with a "#" is a comment,
+# which is ignored by the Python interpreter.
+
+print('Hello world')
+
